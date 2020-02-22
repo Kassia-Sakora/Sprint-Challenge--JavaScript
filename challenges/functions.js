c67144a405d4db056550247e7129e6be16210eb7
Julie Key <juliekey1991@gmail.com>
2020-02-21 22:21:32 -0700

    answered questions in README

--- challenges/functions.js
+++ challenges/functions.js
@@ -37,7 +37,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Closures allow you to access one function from inside another function.
 
 
 const external = "I'm outside the function";
