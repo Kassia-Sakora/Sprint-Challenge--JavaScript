06ff1133340ddfc00c6d81825310321812ba726c
Julie Key <juliekey1991@gmail.com>
2020-02-21 21:29:14 -0700

    completed objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -75,21 +75,41 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+function getUniversities(u) {
+  const universities = [];
+  u.forEach(function(item) {
+    universities.push(item.university);
+  })
+  return universities;
+}
+console.log(getUniversities(graduates).sort());
 
+
+// const sort = u.sort();
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
-console.log(contactInfo);
+function getContact(c) {
+  const contactInfo = [];
+  c.forEach(function(item) {
+    contactInfo.push(`${item.first_name} ${item.email}`);
+  })
+  return contactInfo;
+}
+console.log(getContact(graduates));
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter((item) => {
+  return item.university.includes("Uni")
+});
+
 console.log(unisWithUni);
+
+// const unisWithUni = [];
+// console.log(unisWithUni);
 
 
 // ==== ADVANCED Array Methods ====
@@ -114,32 +134,58 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
-console.log(displayNames);
+function getNames(n) {
+  const displayNames = [];
+  n.forEach(function(item) {
+    displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}.`);
+  })
+  return displayNames;
+}
+console.log(getNames(zooAnimals));
+
+
+// const displayNames = [];
+// console.log(displayNames);
 
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
+function getLower(l) {
+  const lowCaseAnimalNames = l.map(function(item) {
+   return item.animal_name.toLowerCase();    
+  })
+  return lowCaseAnimalNames;
+}
+console.log(getLower(zooAnimals));
 
-const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+// const lowCaseAnimalNames = [];
+// console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter((item) => {
+  return item.population < 5
+});
+
 console.log(lowPopulationAnimals);
+// const lowPopulationAnimals = [];
+// console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((total, item) => {
+  return total += item.population;
+}, 0);
+
+// const populationTotal = 0;
 console.log(populationTotal);
 
 
